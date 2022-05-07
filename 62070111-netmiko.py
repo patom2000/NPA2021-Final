a4a0fa4900045ac91ef74a11a6d32609c19e4538
PWic2510 <patompong909@gmail.com>
2022-05-07 15:11:59 +0700

    full function script part 2

--- 62070111-netmiko.py
+++ 62070111-netmiko.py
@@ -11,14 +11,33 @@ device_param = {'device_type': "cisco_ios",
 config_loopback = ["int loopback 62070111", "ip address 192.168.1.1 255.255.255.0"]
 delete_loopback = ["no int loopback 62070111"]
 
-def sendcommand(device):
+def sendloopbackcommand(device, command):
     with ConnectHandler(**device) as ssh:
-        result = ssh.send_config_set(delete_loopback)
+        result = ssh.send_config_set(command)
+        result = output_text_to_list(ssh.send_command("sh ip int br"))
         print(result)
 
-        ip = ssh.send_command("sh ip int br")
-        return ip
+def output_text_to_list(text):
+    return text.strip().split("\n")
 
-def check_loopback():
+def check_loopback(device, readytodel = 0, readytoconfig = 0):
+    with ConnectHandler(**device) as ssh:
+        result = output_text_to_list(ssh.send_command("sh ip int br"))
+        result.pop(0)
+        interface = result[-1].split()
+        if "Loopback" in interface[0]:
+            readytodel = 1
+        if ("Loopback" in interface[0] and interface[1] == "unassigned") or "Loopback" not in interface[0]:
+            readytoconfig = 1
+        if readytoconfig:
+            ans = input("Do you want to create loopback?(Y or N): ")
+            if ans == "y" or ans == "Y":
+                sendloopbackcommand(device, config_loopback)
+            readytodel = 1
+            readytoconfig = 0
+        if readytodel:
+            ans = input("Do you want to delete loopback?(Y or N): ")
+            if ans == "y" or ans == "Y":
+                sendloopbackcommand(device, delete_loopback)
 
-sendcommand(device_param)
+check_loopback(device_param)
