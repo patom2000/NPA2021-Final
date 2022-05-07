719c6bd3b2f4af3d1ea08b6106e28f42153a8d89
PWic2510 <patompong909@gmail.com>
2022-05-07 17:39:15 +0700

    bot first version

--- 62070111-bot.py
+++ 62070111-bot.py
@@ -9,37 +9,28 @@ headers = {
  'Authorization': f'Bearer {access_token}',
  'Content-Type': 'application/json'
 }
+main_room = "Y2lzY29zcGFyazovL3VzL1JPT00vNjUwODkzMjAtY2QxOS0xMWVjLWE1NGUtNGQ2MmNhMWM4YmVl"
+test_room = "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNTg4MjliOTAtOTE5Mi0xMWVjLWI3ZjMtODFlZDdmNGJiODhm"
 
-def add_message(message):
+def add_message(message, room_id):
     url = 'https://webexapis.com/v1/messages'
-    room_id = "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNTg4MjliOTAtOTE5Mi0xMWVjLWI3ZjMtODFlZDdmNGJiODhm"
     params = {'roomId': room_id, 'markdown': message}
-    res = requests.post(url, headers=headers, json=params)
-    print(res.json())
+    requests.post(url, headers=headers, json=params)
 
 def list_room():
     url = "https://webexapis.com/v1/rooms/"
-    headers = {
-    "Authorization": f'Bearer {access_token}',
-    "Content-Type": "application/json"
-    }
     params = {
         "max":'100'
         }
     res = requests.get(url, headers=headers, params=params)
     print(json.dumps(res.json(), indent=4))
 
-def get_message():
+def get_message(room_id):
     amount = 1
-    url = f"https://webexapis.com/v1/messages?roomId=Y2lzY29zcGFyazovL3VzL1JPT00vNjUwODkzMjAtY2QxOS0xMWVjLWE1NGUtNGQ2MmNhMWM4YmVl&max={amount}"
-
-    headers = {
-        "Authorization": f'Bearer {access_token}',
-        "Content-Type": "application/json"
-    }
+    url = f"https://webexapis.com/v1/messages?roomId={room_id}&max={amount}"
     res = requests.get(url, headers=headers)
     response_json = res.json()
-    
+    return response_json['items'][0]['text']
 
 def check_loopback_status():
     device_ip = "10.0.15.105"
@@ -51,10 +42,20 @@ def check_loopback_status():
     resp = requests.get(api_url, auth=basicauth, headers=headers, verify=False)
     response_json = resp.json()
     status = response_json['ietf-interfaces:interfaces']['interface'][-1]['enabled']
+    name = response_json['ietf-interfaces:interfaces']['interface'][-1]['name']
+    return status, name
 
-
-"""add_message("6207011")
+def core_bot(room_id):
+    add_message("start", room_id)
+    while 1:
         time.sleep(1)
-        Y2lzY29zcGFyazovL3VzL1JPT00vNjUwODkzMjAtY2QxOS0xMWVjLWE1NGUtNGQ2MmNhMWM4YmVl"""
+        text = get_message(room_id)
+        if text == "62070111":
+            status, name = check_loopback_status()
+            if status == 1:
+                add_message(f"{name} - Operational status is up", room_id)
+            if status == 0:
+                add_message(f"{name} - Operational status is down", room_id)
 
-get_message()
+
+core_bot(test_room)
